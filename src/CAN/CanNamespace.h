89b3aacff550463b6dc5c555b5576f0397006205
Moritz Herzog <moritz.herzog@german-software-engineers.com>
2019-06-19 14:43:49 +0200

    Adding Error Codes

--- src/CAN/CanNamespace.h
+++ src/CAN/CanNamespace.h
@@ -7,18 +7,30 @@
 
 namespace CAN{
     enum CanID{
-        WheelRearRight,
-        WheelRearFront,
-        WheelFrontLeft,
-        WheelFrontRight,
-        BatteryVoltage,
-        AccelerationX,
-        AccelerationY
+        WheelFrontRight = 0x008,
+        WheelFrontLeft = 0x009,
+        WheelRearLeft = 0x00a,
+        WheelRearRight = 0x00b,
+        BatteryVoltage = 0x00d,
+        AccelerationLongitudinal = 0x190,
+        AccelerationLateral = 0x191,
+        Tempreture = 0x192
     };
 
     enum CanErrorCode{
-        NoError = 0
-
+        NoError = 0,
+        KernelError,
+        ArgError,
+        AccessError,
+        AddressError,
+        NetworkError,
+        MemoryError,
+        BufferError,
+        ReadError = 10,
+        SendError,
+        Connection = 16,
+        UnkownError = 19,
+        TimeOut
     };
 }
 
