491f147bd2ccdd2c464e97b7df36cf2432562798
Moritz Herzog <moritz.herzog@german-software-engineers.com>
2019-07-20 11:17:14 +0200

    Adding class Device Propertys for Diffrent Schedules for each Device

---
+++ src/Scheduler/DevicePropertys.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 20.07.19.
+//
+
+#include "DevicePropertys.h"
---
+++ src/Scheduler/DevicePropertys.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 20.07.19.
+//
+
+#ifndef UI_DEVICEPROPERTYS_H
+#define UI_DEVICEPROPERTYS_H
+
+
+class DevicePropertys {
+
+};
+
+
+#endif //UI_DEVICEPROPERTYS_H
