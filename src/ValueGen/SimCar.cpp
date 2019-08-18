a1ddd8da49129638d362ff258334b4590bc40a9e
Philipp Lersch <plersch@rhrk.uni-kl.de>
2019-08-18 23:31:37 +0200

    Added Car-Specs Support Added more Values

--- src/ValueGen/SimCar.cpp
+++ src/ValueGen/SimCar.cpp
@@ -6,6 +6,7 @@
 #include "SimCar.h"
 
 int main(){
+    //TODO Car Konstanten hinzufügen
     auto* simCar = new ValueGen::SimCar();
     int* nr8 = simCar->getNextSensorValues(8, 1000000);
     int* nr9 = simCar->getNextSensorValues(9, 1000000);
@@ -19,11 +20,12 @@ namespace ValueGen {
     SimCar::SimCar() {
         //Wheelspeed 8-11 = 0x008-0x00b
         // O*65536 + H*256 + L = pulsetime = (24.000.000cm*PI)/wheelspeed cm/50ms?
-        // rechte Radseite +- Kurvenradius
-        // hinteren Räder +- traktionberechnung
-        Sensor main = Sensor(1000000, 0, 0, 0, 0, 10, 0);
-        Sensor curve= Sensor(1000000, 0, 0, 0, 0, 7, 0);
-        Sensor back = Sensor(1000000, 0, 0, 0, 0, 2, 0);
+        // main_Range: -30 bis 30
+        // rechte Radseite +- Kurvenradius: -5 bis 5
+        // hinteren Räder +- traktionberechnung: -10 bis +10
+        Sensor main = Sensor(1000000, -25132741228718, 25132741228718, -376991118, 376991118, 10, 100);
+        Sensor curve= Sensor(1000000, -150796447, 150796447, -753982236, 753982236, 7, 30);
+        Sensor back = Sensor(1000000, -25132741228718, 25132741228718, -150796447, 150796447, 20, 10);
         Sensor *sensor = Sensor::generateWheelspeeds(main, curve, back);
         addSensor(8, &sensor[0]);
         addSensor(9, &sensor[1]);
@@ -35,15 +37,28 @@ namespace ValueGen {
         //  Range: Von 15V auf 10V => 553 - 830
         addSensor(13, new Sensor(1000000, 553, 830, -2, 1, 0, 278));
 
-        //Accel_X 0x190 = 400 TODO +AccelY mit cos statt sin
+        //TODO füllen
+        //Accel_X 0x190 = 400
         //  RAW = sin(x)*256+2048
         //  Range: Von ? bis ? => ? - ?
         addSensor(400, new Sensor(1000000, 0, 0, 0, 0, 3, 0));
+        //Accel_Y 0x191 = 401
+        //  RAW = cos(x)*256+2048
+        //  Range: Von ? bis ? => ? - ?
+        addSensor(400, new Sensor(1000000, 0, 0, 0, 0, 3, 0));
 
+        //TODO: add -40 bis 0
         //Temp 0x192 = 402
         //  RAW = temp*4
         //  !!Range nur 0-120, 40-1 entfällt!!
         addSensor(402, new Sensor(1000000, 0, 120, -1, 1, 5, 10));
+    }
+
+    void SimCar::addSensor(int id, Sensor* sensor) {
+        sensorMap.insert(std::pair<int, Sensor*>(id, sensor));
+    }
+    void SimCar::addAttribute(int id, float* value){
+        attributeMap.insert(std::pair<int, float*>(id, float))
     }
 
     int* SimCar::getNextSensorValues(int sensor, int count) {
@@ -51,9 +66,7 @@ namespace ValueGen {
         assert(iterMap.at(sensor)+count < sensorMap.at(sensor)->getCount());
         return &(sensorMap.at(sensor)->getData()[iterMap.at(sensor)]);
     }
-
-    void SimCar::addSensor(int id, Sensor *sensor) {
-        sensorMap.insert(std::pair<int, Sensor*>(id, sensor));
+    float* SimCar::addAttribute(int id){
+        return attributeMap.at(id);
     }
-
 }
--- src/ValueGen/SimCar.h
+++ src/ValueGen/SimCar.h
@@ -13,11 +13,15 @@ namespace ValueGen {
     public:
         SimCar();
 
+        void addSensor(int id, Sensor* sensor);
+        void SimCar::addAttribute(int id, float* value);
+
+        float* SimCar::addAttribute(int id);
         int* getNextSensorValues(int sensor, int count);
-        void addSensor(int id, Sensor *sensor);
     private:
         std::map<int, Sensor*> sensorMap;
-        std::map<int, int> iterMap;
+        std::map<int, int*> iterMap;
+        std::map<int, float*> attributeMap;
     };
 }
 
