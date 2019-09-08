9186ddb889c918460515fdc3fa93ee969a3f67ca
Moritz Herzog <moritz.herzog@german-software-engineers.com>
2019-09-08 10:30:38 +0200

    New Static Scheduler for POCL Testing on PI

--- src/Scheduler/NewStaticScheduler.cpp
+++ src/Scheduler/NewStaticScheduler.cpp
@@ -10,32 +10,67 @@ using namespace SCHEDULER;
 NewStaticScheduler::NewStaticScheduler(std::vector<Task *> tasks, std::vector<Device *> device) :
         Scheduler(tasks, device) {
     ErrorCode = 1;
+    generateAllPrograms();
 }
 
 
 void NewStaticScheduler::schedule() {
-    for (Device* device : Devices) {
-//		cout << "Device Name: " << device->getName()<<endl;
-        cl::CommandQueue commandQueue(device->getDeviceContext(), device->getOclDevice(), CL_QUEUE_PROFILING_ENABLE);
-        CommandQueues.push_back(commandQueue);
-        for (Task* task : Tasks) {
-//		    cout << "Task Name: "<<task->getKernelName()<<endl;
-            device->generateProgramm(task);
+    for (Device *device : Devices) {
+        TasksToSchedule = Tasks;
+        std::vector<cl::CommandQueue> commandQueues;
 
-            cl::Kernel kernel = cl::Kernel(task->getProgramm(), task->getKernelName().c_str(),  &ErrorCode);
-            if (ErrorCode == CL_SUCCESS) {
-                setRAMForCurrentTask(task, device, kernel, commandQueue);
-                readConstantsFromTask(task, device, kernel, commandQueue);
-                setRAMBufferForOutput(task, device, kernel);
-                setKernelLoad(task, device, kernel);
-                commandQueue.finish();
-                enqueueTak(task, device, commandQueue, kernel);
-                commandQueue.finish();
-                readDataFromTask(task, commandQueue);
+        while(!TasksToSchedule.empty()) {
+            getQueueTasksWithNoDependencies();
+            std::vector<cl::Event> events;
+            for (int i = 0; i < TasksToScheduleInStep.size(); i++) {
+                commandQueues.push_back(cl::CommandQueue(device->getDeviceContext(), device->getOclDevice()));
+                cl::Event currentEvent;
+                events.push_back(currentEvent);
+                Task *task = TasksToScheduleInStep.at(i);
+                cl::Kernel kernel = cl::Kernel(task->getProgramm(), task->getKernelName().c_str(), &ErrorCode);
+                if (ErrorCode == CL_SUCCESS) {
+                    SCHEDULER::Scheduler::setRAMForCurrentTask(task, device, kernel, commandQueues.at(i));
+                    readConstantsFromTask(task, device, kernel, commandQueues.at(i));
+                    setRAMBufferForOutput(task, device, kernel);
+                    setKernelLoad(task, device, kernel);
+                    enqueueTak(task, device, commandQueues.at(i), kernel,currentEvent);
+                    TasksToReadInStep.push_back(task);
+                } else
+                    std::cout << "First Step Kernel Creation Resolved Error: " << ErrorCode << std::endl;
             }
-            else
-                std::cout << "Kernel Creation Resolved Error: " << ErrorCode<<std::endl;
+            cl::Event::waitForEvents(events);
+            for (int i = 0; i<TasksToReadInStep.size(); i++) {
+                readDataFromTask(TasksToReadInStep.at(i), commandQueues.at(i));
+            }
+            TasksToReadInStep.clear();
         }
         deleteAllBuffers();
     }
+}
+
+void NewStaticScheduler::getQueueTasksWithNoDependencies() {
+    bool TasksAreRemoved = true;
+    while (TasksAreRemoved) {
+        TasksAreRemoved = false;
+        for (int i = 0; i < TasksToSchedule.size(); i++) {
+            if (TasksToSchedule.at(i)->dependenciesAreCalculated()) {
+                TasksToScheduleInStep.push_back(TasksToSchedule.at(i));
+                TasksToSchedule.erase(TasksToSchedule.begin() + i);
+                TasksAreRemoved = true;
+            }
+            i++;
+        }
+    }
+//    std::cout << "Tasks to Schedule: " << TasksToSchedule.size() << endl;
+}
+
+void NewStaticScheduler::generateAllPrograms() {
+    for (Device *device : Devices)
+        for (Task *task : Tasks)
+            device->generateProgramm(task);
+}
+
+void NewStaticScheduler::enqueueTak(Task *task, Device *device, cl::CommandQueue commandQueue, cl::Kernel kernel, cl::Event &event) {
+    ErrorCode = commandQueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(CoreCount), cl::NDRange(CoreCount),NULL,&event);
+//    std::cout << "Enqueue Task: " << ErrorCode << std::endl;
 }
--- src/Scheduler/NewStaticScheduler.h
+++ src/Scheduler/NewStaticScheduler.h
@@ -17,7 +17,16 @@ namespace SCHEDULER {
         NewStaticScheduler(std::vector<Task*> tasks, std::vector<Device*> device);
         void schedule() override;
 
+    protected:
+        void enqueueTak(Task* task, Device *device, cl::CommandQueue commandQueue, cl::Kernel kernel, cl::Event &event);
+
     private:
+        void getQueueTasksWithNoDependencies();
+        void generateAllPrograms();
+
+        std::vector<Task*> TasksToScheduleInStep;
+        std::vector<Task*> TasksToReadInStep;
+        std::vector<Task*> TasksToSchedule;
     };
 }
 
